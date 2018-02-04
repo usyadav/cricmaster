15d8aaeee20a58207053457cf846d38150b68bcc
usyadav <umashankar.y@HCL.COM>
2018-02-04 17:09:49 +0530

    player team databinding modules added

---
+++ src/app/data-binding/binding-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-binding-demo',
+  templateUrl: './binding-demo.component.html',
+  styleUrls: ['./binding-demo.component.css']
+})
+export class BindingDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
