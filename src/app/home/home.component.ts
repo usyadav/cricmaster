4e5166ee7ffb53e0b098845b8f1a5bc0400b35b6
aksghankar <akgshankar@gmail.com>
2018-04-03 18:57:21 +0530

    Routing from component class is added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router:Router) { }
 
   ngOnInit() {
+
   }
 
+  goToTeams(){
+    this._router.navigateByUrl('/team');
+    
+  }
+
+  goToDirectiveDemos(){
+    this._router.navigate(['demos/customdirective'])
+  }
 }
