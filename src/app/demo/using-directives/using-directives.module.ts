6f57626e11d63ef08f9dc07b9948b6b4c5c5eef6
aksghankar <akgshankar@gmail.com>
2018-04-03 21:16:32 +0530

    using pipes in component class

--- src/app/demo/using-directives/using-directives.module.ts
+++ src/app/demo/using-directives/using-directives.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { DirectiveDemoComponent } from './directive-demo.component';
 import { SharedModule } from './../../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,SharedModule
+    CommonModule, SharedModule, FormsModule
   ],
   declarations: [DirectiveDemoComponent],
-  exports:[DirectiveDemoComponent]
+  exports: [DirectiveDemoComponent]
 })
 export class UsingDirectivesModule { }
